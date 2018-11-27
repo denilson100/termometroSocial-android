a0f5cd906d9102c715edb955703f8869beecd209
Denilson Monteiro <denilson100@gmail.com>
2018-11-27 07:40:22 -0200

    add commit teste 8

--- app/src/main/java/br/com/mobile10/termometrosocial/MainActivity.kt
+++ app/src/main/java/br/com/mobile10/termometrosocial/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // teste 7
+        // teste 8
     }
 }
