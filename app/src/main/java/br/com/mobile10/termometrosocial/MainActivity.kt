0b3abcb46f953f8ff2b0f8120f569d44dcaec057
Denilson Monteiro <denilson100@gmail.com>
2018-12-03 07:36:58 -0200

    Merge master

--- app/src/main/java/br/com/mobile10/termometrosocial/MainActivity.kt
+++ app/src/main/java/br/com/mobile10/termometrosocial/MainActivity.kt
@@ -8,11 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
         // teste 18
-=======
-        // teste 15
->>>>>>> master
 
     }
 }
